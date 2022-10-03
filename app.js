66ec76abb1657f65b8a97a1e56ceca22207fbd9d
bluelightning40 <shoe.laundry00@gmail.com>
2022-10-03 23:48:24 +0700

    updating API port

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ require('dotenv').config()
 
 const app = express()
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 // app.use('/uploads', express.static('uploads'))
