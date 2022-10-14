f2a72b5cea0f215819d3876870850fa46778f1bc
bluelightning40 <114933053+shoelaundry00@users.noreply.github.com>
2022-10-14 22:03:04 +0700

    debug employee privilege 2

--- routes/employee.js
+++ routes/employee.js
@@ -199,6 +199,7 @@ router.put('/update/:id', async (req, res, next) => {
 
   const connection = await db.getConnection()
   try {
+    console.log("try updating employee")
     privilegeChecks(req.loggedPrivileges, requiredPrivileges, req.loggedIsAdmin)
 
     const { name, password, note, privileges } = req.body
